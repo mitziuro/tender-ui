f65760ad2245e6955c2a2427e16d3a5595b92ced
el.gato.mexicano <el.gato.mexicano@viva.mexico.mx>
2020-02-02 22:08:55 +0200

    more updates

--- src/app/crud/tender/search.notice.crud.js
+++ src/app/crud/tender/search.notice.crud.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import _SERVER_URL from 'base.server';
+import { _SERVER_URL } from './base.server';
 
 //export const SERVER_URL = 'http://51.140.219.10:8080';
 export const SERVER_URL = _SERVER_URL;
